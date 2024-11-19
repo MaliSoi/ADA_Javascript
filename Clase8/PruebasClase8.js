969f8f257d339e4e9afea036f99f4004fa59f42b
MaliSoi <malalasoifer@gmail.com>
2024-11-19 13:37:21 -0300

    agregar nuevos

--- Clase8/PruebasClase8.js
+++ Clase8/PruebasClase8.js
@@ -1,3 +1,38 @@
+// Probando temas de la clase 8 
+const prompt = require("prompt-sync")({ sigint: true });
+
+let nombre = 'Malala';
+const edad = 43
+nombre = 'Agos'
+console.log(`Hola mi nombre es ${nombre} y tengo ${edad} anios`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
