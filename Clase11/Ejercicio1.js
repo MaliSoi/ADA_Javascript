2be24a2fb13344faec323e708d30464076088763
MaliSoi <malalasoifer@gmail.com>
2024-11-26 09:39:56 -0300

    Guardar cambios

--- Clase11/Ejercicio1.js
+++ Clase11/Ejercicio1.js
@@ -5,3 +5,23 @@
 
 const prompt = require('prompt-sync')();
 
+let numero = parseFloat(prompt("Ingresa un número:"));
+
+//Una manera puede ser asi:
+
+// if (numero > 0) {
+//    console.log ("El número es positivo.");
+//} else if (numero < 0) {
+//   console.log ("El número es negativo.");
+//} else {
+//    console.log ("El número es 0.");
+//}
+
+//Otra manera, if ternario:
+
+let mensaje1 = numero > 0 ? 'es positivo.': 'es negativo.';
+let mensaje2 = numero == 0 ? 'es cero.' : mensaje1 ;
+
+console.log ("El resultado", mensaje2);
+
+
