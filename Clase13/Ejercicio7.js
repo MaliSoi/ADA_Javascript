f1cdf44fda3ee811137cbb66feb9704100ab550e
MaliSoi <malalasoifer@gmail.com>
2024-12-10 21:26:57 -0300

    Agregar archivo Ejecicio7.js

---
+++ Clase13/Ejercicio7.js
@@ -0,0 +1,21 @@
+//Mensaje según la hora
+//mostrarSaludo que imprima "Buenos días" si la hora es menor a 12,
+// "Buenas tardes" si es menor a 18, o "Buenas noches" en cualquier otro caso.
+
+const prompt = require('prompt-sync')()
+
+//me falto una variable
+
+let hora = 21
+
+function mostrarSaludo (){
+    if(hora < 12){
+        console.log  ("Buenos días");
+    } else if (hora < 18){
+        console.log ("Buenas tardes");
+    }else{
+        console.log ("Buenas noches")
+    }
+    }
+
+   mostrarSaludo ();
