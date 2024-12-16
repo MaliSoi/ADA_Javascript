b3b7b2954e8c9c5df154f3daa099b060fc08deac
MaliSoi <malalasoifer@gmail.com>
2024-12-16 00:58:03 -0300

    Agregar Ejercicio1.js

---
+++ Clase15/Ejercicio1.js
@@ -0,0 +1,21 @@
+//Añadir personajes a una lista
+//Tienes una lista de personajes de videojuegos ["Link", "Zelda"]. 
+//Agrega los personajes "Mario" y "Luigi" al final de la lista utilizando el método adecuado. 
+// Luego, muestra el array actualizado.
+
+const prompt = require('prompt-sync')()
+
+let personajesVideoJuegos = ['Link', 'Zelda'];
+personajesVideoJuegos.push ('Mario', 'Luigi');
+console.log( personajesVideoJuegos);
+
+
+
+
+
+
+
+
+
+
+
