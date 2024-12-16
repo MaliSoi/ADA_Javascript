6e89216ae9de9762b41c80db248f37cb6b8bef80
MaliSoi <malalasoifer@gmail.com>
2024-12-16 01:52:14 -0300

    Agregar Ejercicio8.js

---
+++ Clase15/Ejercicio8.js
@@ -0,0 +1,13 @@
+//Extraer una parte de una frase.
+//Tienes la frase "Los gatos son geniales".
+//Extrae y muestra solo la parte "gatos" utilizando el método adecuado.
+
+
+const prompt = require('prompt-sync')()
+
+let frase = "Los gatos son geniales"
+ 
+console.log (frase.slice(4, 10));
+
+
+
