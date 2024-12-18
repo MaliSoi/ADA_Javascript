bf1e0037aa1f1568d1bd668646aec4af1ed48865
MaliSoi <malalasoifer@gmail.com>
2024-12-18 09:00:07 -0300

    Agregar Ejercicio5.js

---
+++ Clase16/Ejercicio5.js
@@ -0,0 +1,12 @@
+//Recorre una lista e imprime cada elemento.
+//Tienes una lista de frutas ['manzana', 'banana', 'cereza']. 
+// Tu tarea es recorrer la lista y mostrar el nombre de cada fruta en la consola, una por una.
+
+const prompt = require('prompt-sync')()
+
+let frutas = ['manzana', 'banana', 'cereza']
+
+frutas.forEach(function(frutas){
+    console.log(frutas);
+});
+
